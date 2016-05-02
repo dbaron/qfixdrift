86b0d07ab3759d67bea7cc8ce26b884d5edb39f0
L. David Baron <dbaron@dbaron.org>
2016-05-02 09:28:52 -0700

    Since @command decorator goes back to 1.9, restore my old testedwith data (without testing in anything old other than 3.2.3).

--- qfixdrift.py
+++ qfixdrift.py
@@ -21,7 +21,7 @@ from mercurial.node import hex, short
 from mercurial import util, cmdutil, scmutil
 from hgext.mq import patchheader
 
-testedwith = '3.8.1'
+testedwith = '1.9 2.0 2.1 2.5 2.6 2.9 3.1 3.2 3.3 3.5 3.6 3.8.1'
 cmdtable = {}
 command = cmdutil.command(cmdtable)
 
